395f712f0804f4a2b188208fe0f4e56f4b3b8862
anaconduck <wahyukusuma29@gmail.com>
2026-04-09 14:07:34 +0700

    add enter-3

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ st.title("🥚 Sistem Klasifikasi Kualitas Telur")
 st.markdown("Sistem Berbasis AI Untuk Klasifikasi Kualitas Telur Berdasarkan Warna Menggunakan Deep Learning.")
 
 st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("### 🧾 Kategori Klasifikasi")
+st.markdown("### Kategori Klasifikasi:")
 
 st.image("jenis-telur.png",width=500)
 
