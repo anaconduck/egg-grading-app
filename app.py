3c488e81a685b16b9119a28357d2dbf1a571809b
anaconduck <wahyukusuma29@gmail.com>
2026-04-09 14:08:20 +0700

    add

--- app.py
+++ app.py
@@ -146,7 +146,6 @@ mode = st.sidebar.radio(
 st.title("🥚 Sistem Klasifikasi Kualitas Telur")
 st.markdown("Sistem Berbasis AI Untuk Klasifikasi Kualitas Telur Berdasarkan Warna Menggunakan Deep Learning.")
 
-st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("### Kategori Klasifikasi:")
 
 st.image("jenis-telur.png",width=500)
