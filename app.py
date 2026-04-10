7955629426426a87a09f96deceab700ccc11681b
anaconduck <wahyukusuma29@gmail.com>
2026-04-10 10:52:36 +0700

    delete it

--- app.py
+++ app.py
@@ -172,8 +172,7 @@ st.markdown("Sistem Berbasis AI Untuk Klasifikasi Kualitas Telur Ayam Berdasarka
 st.markdown("<br>", unsafe_allow_html=True)
 st.markdown("#### Kategori Klasifikasi:")
 
-#st.image("jenis-telur.png",width=500)
-st.image("jenis-telur2.png",width=800)
+st.image("jenis-telur.png",width=500)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
